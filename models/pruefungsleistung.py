74f546bec9ba0f367732544b97854248a1563415
MikaIU97 <mika.stocksiefen@iu-study.org>
2026-06-30 15:53:23 +0200

    Phase 3: Code dokumentiert und verbessert
    Added a description docstring to the Pruefungsleistung class and included a 'versuch' attribute.

--- models/pruefungsleistung.py
+++ models/pruefungsleistung.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 
 @dataclass
 class Pruefungsleistung:
+    """Repräsentiert eine Prüfungsleistung innerhalb eines Studiums, einschließlich der Modul-ID, des Modulnamens, des Datums, der Note, der ECTS-Punkte, des Bestehensstatus und des Versuchs."""
     modul_name: str
     datum: str | None
     note: float | None
